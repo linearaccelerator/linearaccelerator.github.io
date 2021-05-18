fad39a8f9563d7e6cd128f38433f3e9e2021c4eb
linearaccelerator <linearaccelerator@github.io>
2021-05-18 19:01:33 +0200

    First upgrade

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ let pickaxes = [
 
 let defaultSave = {
     money: 0,
-    pickaxe: "Paper Pickaxe"
+    pickaxe: "Paper Pickaxe",
+    upgrades: {
+        muscle: [0, 50]
+    }
 }
 
 let save;
@@ -17,11 +20,27 @@ save = defaultSave;
 function mine() {
     let pickaxe = pickaxes.find(p => p.name === save.pickaxe);
     let gain = pickaxe.power;
-    gain += Number((Math.random(pickaxe.maxBonus)*.25).toFixed(2));
+    gain += Number((Math.random()*pickaxe.maxBonus).toFixed(2));
+    gain += 0.2 * save.upgrades.muscle[0]
     save.money += gain;
     updateText()
 }
 
+function buy(itemType){
+    switch(itemType){
+        case 1:
+            if(!(save.money >= save.upgrades.muscle[1])) return;
+            save.money -= save.upgrades.muscle[1];
+            save.upgrades.muscle[0]++;
+            save.upgrades.muscle[1] *= 1.2;
+            break;
+    }
+    updateText();
+}
+
 function updateText() {
     document.getElementById("coin-display").innerText = `${save.money.toFixed(2)} coins`;
+    document.getElementById("muscle-button").innerText = `(${save.upgrades.muscle[0]}) ${save.upgrades.muscle[1].toFixed(2)} coins`;
 }
+
+setInterval(() => updateText(), 500)
