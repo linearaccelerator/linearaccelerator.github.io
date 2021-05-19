8512ecf931b265a5b15fbb6479acfcb19c502c9c
linearaccelerator <linearaccelerator@github.io>
2021-05-19 21:31:16 +0200

    Complete rework

--- index.js
+++ index.js
@@ -1,48 +1,38 @@
-let pickaxes = [
-    {
-        name: "Paper Pickaxe",
-        power: 1,
-        maxBonus: .25
-    }
-]
-
 let defaultSave = {
     money: 0,
-    pickaxe: "Paper Pickaxe",
-    upgrades: {
-        muscle: [0, 50],
-        slavery: [0, 250]
-    }
+    upgrades: [[0, 15], [0, 100], [0, 500]]
 }
 /**
  * @type {defaultSave}
  */
 let save;
+let fps = 15;
 save = defaultSave;
 
 function mine() {
-    let pickaxe = pickaxes.find(p => p.name === save.pickaxe);
-    let gain = pickaxe.power;
-    gain += Number((Math.random()*pickaxe.maxBonus).toFixed(2));
-    gain += 0.25 * save.upgrades.muscle[0];
-    gain += 0.15 * save.upgrades.slavery[0];
-    save.money += gain;
+    save.money++;
     updateText();
 }
 
 function buy(itemType){
     switch(itemType){
         case 1:
-            if(!(save.money >= save.upgrades.muscle[1])) return;
-            save.money -= save.upgrades.muscle[1];
-            save.upgrades.muscle[0]++;
-            save.upgrades.muscle[1] *= 1.2;
+            if(save.money < save.upgrades[0][1]) return;
+            save.money -= save.upgrades[0][1];
+            save.upgrades[0][0]++;
+            save.upgrades[0][1] *= 1.15;
             break;
         case 2:
-            if(!(save.money >= save.upgrades.slavery[1])) return;
-            save.money -= save.upgrades.slavery[1];
-            save.upgrades.slavery[0]++;
-            save.upgrades.slavery[1] *= 1.2;
+            if(save.money < save.upgrades[1][1]) return;
+            save.money -= save.upgrades[1][1];
+            save.upgrades[1][0]++;
+            save.upgrades[1][1] *= 1.2;
+            break;
+        case 3:
+            if(save.money < save.upgrades[2][1]) return;
+            save.money -= save.upgrades[2][1];
+            save.upgrades[2][0]++;
+            save.upgrades[2][1] *= 1.25;
             break;
     }
     updateText();
@@ -50,11 +40,14 @@ function buy(itemType){
 
 function updateText() {
     document.getElementById("coin-display").innerText = `${save.money.toFixed(2)} coins`;
-    document.getElementById("muscle-button").innerText = `(${save.upgrades.muscle[0]}) ${save.upgrades.muscle[1].toFixed(2)} coins`;
-    document.getElementById("slavery-button").innerText = `(${save.upgrades.slavery[0]}) ${save.upgrades.slavery[1].toFixed(2)} coins`;
+    document.getElementById("auto-miner-btn").innerText = `(${save.upgrades[0][0].toFixed(2)}) ${save.upgrades[0][1].toFixed(2)} coins`;
+    document.getElementById("gen-1-btn").innerText = `(${save.upgrades[1][0].toFixed(2)}) ${save.upgrades[1][1].toFixed(2)} coins`;
+    document.getElementById("gen-2-btn").innerText = `(${save.upgrades[2][0].toFixed(2)}) ${save.upgrades[2][1].toFixed(2)} coins`;
 }
 
 setInterval(() => {
-    save.money += 0.125 * save.upgrades.slavery[0];
+    save.money += save.upgrades[0][0] * .4 / fps;
+    save.upgrades[0][0] += save.upgrades[1][0] * .1 / fps;
+    save.upgrades[1][0] += save.upgrades[2][0] * .1 / fps;
     updateText();
-}, 250);
+}, 1000 / fps);
